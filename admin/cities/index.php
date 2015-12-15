99451634c2adffbe42794f0924dc68cf575677f5
AVS-Studio <avs@nur.kz>
2015-12-15 09:17:31 +0600

    Правил синтаксис

--- admin/cities/index.php
+++ admin/cities/index.php
@@ -4,34 +4,36 @@
 
     $link = db_connect();
 
-    if(isset($_GET['action']))
-    	$action = $_GET['action'];
-    else
-    	$action = "";
+    if(isset($_GET['action'])){
+        $action = $_GET['action'];
+    } else {
+        $action = "";
+    }
 
     if($action == "add_city"){
     	if(!empty($_POST)){
-    		cities_new($link, $_POST['name'], $_POST['alias']);
-    		header("Location: index.php");
+            cities_new($link, $_POST['name'], $_POST['alias']);
+            header("Location: index.php");
     	}
-	} 
-	elseif($action == "edit-city"){
+    } elseif($action == "edit-city"){
     	echo "edit_city in controller ID=";
     	echo $_GET['id'];
     } elseif($action == "publish-city"){
-    	if(isset($_GET['id']))
-	    	cities_publish($link, $_GET['id']);
-		    header("Location: index.php");
+        if (isset($_GET['id'])) {
+            cities_publish($link, $_GET['id']);
+            header("Location: index.php");
+        }    
     } elseif ($action == "unpublish-city"){
-    	if(isset($_GET['id']))
-	    	cities_unpublish($link, $_GET['id']);
-	    	header("Location: index.php");
+    	if(isset($_GET['id'])){
+            cities_unpublish($link, $_GET['id']);
+            header("Location: index.php");
+        }
     } elseif ($action == "delete-city"){
-    	if(isset($_GET['id']))
-	    	cities_delete($link, $_GET['id']);
-	    	header("Location: index.php");
+    	if(isset($_GET['id'])){
+            cities_delete($link, $_GET['id']);
+            header("Location: index.php");
+        }
     } else {
     	$cities = cities_all($link);
     	include("../views/admin-cities.php");
     }
-?>
