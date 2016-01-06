e24f1d10ad11c191114bf93d9a894d7d9ad1a676
avs-studio <avs@nur.kz>
2016-01-06 16:32:41 +0600

    Начал делать страничку моллов

--- admin/malls/add-mall.php
+++ admin/malls/add-mall.php
@@ -1,6 +1,7 @@
 <?php include_once('../header.php') ?>
 <div class="row edit-buttons">
 	<div class="col-xs-12 pull-xs-right text-xs-right">
+    	<button type="button" class="btn btn-success-outline"><i class="fa fa-plus"></i> Добавить</button>
     	<button type="button" class="btn btn-success-outline"><i class="fa fa-pencil-square-o"></i> Править</button>
     	&nbsp;&nbsp;
     	<button type="button" class="btn btn-primary-outline"><i class="fa fa-plus-square-o"></i> Опубликовать</button>
@@ -10,7 +11,48 @@
     </div>
 </div>
 <hr>
-<br>
-<div class="lead">Добавить молл</div>
-<br>
+<div class="row">
+	<div class="col-md-6">
+		
+	</div>
+	<div class="col-md-6">
+		<div class="lead">Список моллов</div>
+		<br>
+		<table class="table table-hover table-sm mall-list">
+		  <thead>
+		    <tr class="table-active">
+		      <th><input type="checkbox" class="check-all"></th>
+		      <th>№</th>
+		      <th>Название</th>
+		      <th>Город</th>
+		      <th>ID</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		    <tr>
+		      <td><input type="checkbox" name="12"></td>
+		      <td>1</td>
+		      <td>MEGA Astana</td>
+		      <td class="text-muted">Астана</td>
+		      <td>12</td>
+		    </tr>
+		    <tr>
+		      <td><input type="checkbox" name="1"></td>
+		      <td>2</td>
+		      <td>Хан Шатыр</td>
+		      <td class="text-muted">Астана</td>
+		      <td>1</td>
+		    </tr>
+		    <tr>
+		      <td><input type="checkbox" name="5"></td>
+		      <td>3</td>
+		      <td>Сарыарка</td>
+		      <td class="text-muted">Астана</td>
+		      <td>5</td>
+		    </tr>
+		  </tbody>
+		</table>
+	</div>
+</div>
+
 <?php include_once('../footer.php') ?>
